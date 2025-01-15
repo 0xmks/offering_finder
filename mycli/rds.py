00271e90dc1581062c396fb447f9f17df63cc5d3
k1s1eee <17310701+0xmks@users.noreply.github.com>
2025-01-15 18:06:50 +0900

    fix:rds

--- mycli/rds.py
+++ mycli/rds.py
@@ -1,54 +1,64 @@
 import click
 import json
-from src.offering_finder.models.rds_params import RDSParams
+from src.offering_finder.models.rds_params import RDSParams, RDSPurchaseParams
 from src.offering_finder.managers.rds_manager import RDSManager
 
 
 # rds subcommand
 @click.command()
 @click.option(
+    "--purchase_profile",
+    required=False,
+    type=str,
+    help="Purchase command set a named profile (e.g., 'default' 'my-profile')",
+)
+@click.option(
+    "--reserved_instance_offering_id",
+    required=False,
+    type=str,
+    help="Reserved instance offering ID",
+)
+@click.option(
     "--product_description",
     required=True,
-    help="Product description (e.g., 'MySQL' 'PostgreSQL' 'aurora mysql' 'aurora postgresql')"
+    type=str,
+    help="Product description (e.g., 'MySQL' 'PostgreSQL' 'aurora mysql' 'aurora postgresql')",
 )
 @click.option(
     "--db_instance_class",
     required=True,
-    help="DB instance class (e.g., 'db.m5.large' 'db.r5.xlarge' 'db.t3.medium'...)"
+    type=str,
+    help="DB instance class (e.g., 'db.m5.large' 'db.r5.xlarge' 'db.t3.medium'...)",
 )
 @click.option(
-    "--duration",
-    required=True,
-    help="Duration (e.g., 31536000)"
+    "--duration", required=True, type=str, help="Duration (e.g., '31536000' '94608000')"
 )
-@click.option(
-    "--quantity",
-    required=True,
-    type=int,
-    help="Quantity (e.g., 2)"
-)
+@click.option("--quantity", required=True, type=int, help="Quantity (e.g., 2)")
 @click.option(
     "--region_name",
     required=True,
-    help="AWS region name (e.g., 'ap-northeast-1')"
+    type=str,
+    help="AWS region name (e.g., 'ap-northeast-1')",
 )
 @click.option(
-    "--multi_az",
-    is_flag=True,
-    help="Specify if the instance should be Multi-AZ"
+    "--multi_az", is_flag=True, help="Specify if the instance should be Multi-AZ"
 )
 @click.option(
     "--offering_type",
     required=True,
+    type=str,
     help="Offering type (e.g., 'All Upfront', 'Partial Upfront', 'No Upfront')",
 )
 @click.option(
     "--reserved_instance_id",
     required=False,
+    type=str,
     help="Reserved instance ID (optional)",
 )
 def rds(
+    purchase_profile,
     region_name,
+    reserved_instance_offering_id,
     quantity,
     product_description,
     db_instance_class,
@@ -57,16 +67,26 @@ def rds(
     offering_type,
     reserved_instance_id,
 ):
-    """ Retrieve Amazon RDS offerings """
+    """Retrieve Amazon RDS offerings"""
+    manager = RDSManager(region_name=region_name)
+    """ 1. Get all offerings """
     params = RDSParams(
-        product_description=product_description,
-        db_instance_class=db_instance_class,
-        duration=duration,
+        ReservedDBInstanceOfferingId=reserved_instance_offering_id,
+        ProductDescription=product_description,
+        DBInstanceClass=db_instance_class,
+        Duration=duration,
+        MultiAZ=multi_az,
+        OfferingType=offering_type,
+    )
+    offerings = manager.get_offerings(params)
+
+    """ 2. Add Purchase Command and Purchase Offering """
+    purchase_params = RDSPurchaseParams(
+        purchase_profile=purchase_profile,
+        region_name=region_name,
         quantity=quantity,
-        multi_az=multi_az,
-        offering_type=offering_type,
         reserved_instance_id=reserved_instance_id,
     )
-    manager = RDSManager(region_name=region_name)
-    offerings = manager.get_offering_ids(params)
-    print(json.dumps(offerings, indent=2))
+    result = manager.add_keys_to_offerings(offerings, purchase_params)
+
+    print(json.dumps(result, indent=2))
--- src/offering_finder/models/rds_params.py
+++ src/offering_finder/models/rds_params.py
@@ -1,50 +1,31 @@
-from dataclasses import dataclass
-from typing import Optional, Dict, Any
+from typing import Optional, Any, Dict
+from pydantic import BaseModel
 
 
-@dataclass
-class RDSParams:
+class RDSPurchaseParams(BaseModel):
     """
-    Data class for RDS parameters.
-    https://docs.aws.amazon.com/AmazonRDS/latest/APIReference/API_DescribeReservedDBInstances.html
+    Data class for common parameters.
+    https://awscli.amazonaws.com/v2/documentation/api/latest/reference/rds/purchase-reserved-db-instances-offering.html
     """
-    region_name: Optional[str] = "ap-northeast-1"
-    quantity: Optional[int] = None
-    marker: Optional[str] = None
-    reserved_db_instance_id: Optional[str] = None
-    reserved_db_instances_offering_id: Optional[str] = None
-    db_instance_class: Optional[str] = None
-    duration: Optional[str] = None
-    offering_type: Optional[str] = None
-    multi_az: Optional[bool] = None
-    product_description: Optional[str] = None
-    filters: Optional[list[Dict[str, Any]]] = None
-    max_records: Optional[int] = 100
+
+    purchase_profile: Optional[str] = None
+    region_name: Optional[str] = None
+    quantity: Optional[int] = 1
     reserved_instance_id: Optional[str] = None
 
-    def to_dict(self) -> Dict[str, Any]:
-        """
-        Converts the parameters to a dictionary for AWS API calls.
-        """
-        params = {}
-        if self.reserved_db_instance_id:
-            params["ReservedDBInstanceId"] = self.reserved_db_instance_id
-        if self.reserved_db_instances_offering_id:
-            params["ReservedDBInstancesOfferingId"] = self.reserved_db_instances_offering_id
-        if self.db_instance_class:
-            params["DBInstanceClass"] = self.db_instance_class
-        if self.duration:
-            params["Duration"] = self.duration
-        if self.offering_type:
-            params["OfferingType"] = self.offering_type
-        if self.multi_az is not None:
-            params["MultiAZ"] = self.multi_az
-        if self.product_description:
-            params["ProductDescription"] = self.product_description
-        if self.filters:
-            params["Filters"] = self.filters
-        if self.max_records:
-            params["MaxRecords"] = self.max_records
-        if self.marker:
-            params["Marker"] = self.marker
-        return params
+
+class RDSParams(BaseModel):
+    """
+    Data class for RDS parameters.
+    https://boto3.amazonaws.com/v1/documentation/api/1.35.6/reference/services/rds/client/describe_reserved_db_instances_offerings.html
+    """
+
+    ReservedDBInstancesOfferingId: Optional[str] = None
+    DBInstanceClass: Optional[str] = None
+    Duration: Optional[str] = "31536000"
+    ProductDescription: Optional[str] = None
+    MultiAZ: Optional[bool] = False
+    OfferingType: Optional[str] = None
+    MaxRecords: Optional[int] = 100
+    Marker: Optional[str] = None
+    Filters: Optional[list[Dict[str, Any]]] = None
