3815a82582e918d0c853cfbbc850fc543e0c0bc3
k1s1eee <17310701+0xmks@users.noreply.github.com>
2025-01-15 19:22:11 +0900

    fix: savingsplans

--- mycli/savingsplans.py
+++ mycli/savingsplans.py
@@ -1,45 +1,33 @@
 import click
 import json
-from src.offering_finder.models.savingsplans_params import SavingsPlansParams
+from src.offering_finder.models.savingsplans_params import (
+    SavingsPlansParams,
+    SavingsPlansPurchaseParams,
+)
 from src.offering_finder.managers.savingsplans_manager import SavingsPlansManager
 
 
 # savingsplans subcommand
 @click.command()
 @click.option(
+    "--purchase_profile",
+    required=False,
+    type=str,
+    help="Purchase command set a named profile (e.g., 'default' 'my-profile')",
+)
+@click.option(
     "--region_name",
     default="ap-northeast-1",
     required=True,
     type=str,
-    help="AWS region name (e.g., 'ap-northeast-1')"
-)
-@click.option(
-    "--commitment",
-    default=0.01,
-    type=float,
-    required=True,
-    help="Commitment (e.g., 0.01)"
-)
-@click.option(
-    "--durations",
-    default=[31536000],
-    multiple=True,
-    type=int,
-    help="Durations in seconds (e.g., 31536000 94608000)"
-)
-@click.option(
-    "--plan_types",
-    default=["Compute"],
-    required=False,
-    multiple=True,
-    type=str,
-    help="Plan type (e.g., 'Compute' 'EC2 Instance' 'EC2 Instance Family' 'Compute Instance' 'EC2 Instance Savings Plan')"
+    help="AWS region name (e.g., 'ap-northeast-1')",
 )
 @click.option(
     "--offering_id",
     required=False,
+    multiple=True,
     type=str,
-    help="Offering ID"
+    help="The IDs of the offerings.",
 )
 @click.option(
     "--payment_options",
@@ -47,33 +35,84 @@ from src.offering_finder.managers.savingsplans_manager import SavingsPlansManage
     required=False,
     multiple=True,
     type=str,
-    help="Offering type (e.g., 'All Upfront', 'Partial Upfront', 'No Upfront')"
+    help="Offering type (e.g., 'All Upfront', 'Partial Upfront', 'No Upfront')",
 )
 @click.option(
-    "--client_token",
+    "--product_type",
     required=False,
     type=str,
-    help="Client token"
+    help="Offering type ('EC2'|'Fargate'|'Lambda'|'SageMaker')",
 )
+@click.option(
+    "--plan_types",
+    default=["Compute"],
+    required=False,
+    multiple=True,
+    type=str,
+    help="Plan type ('Compute'|'EC2Instance'|'SageMaker')",
+)
+@click.option(
+    "--commitment",
+    default=0.01,
+    type=float,
+    required=True,
+    help="Commitment (e.g., 0.01)",
+)
+@click.option(
+    "--currency",
+    type=str,
+    required=False,
+    help="currency ('CNY'|'USD')",
+)
+@click.option(
+    "--durations",
+    default=[31536000],
+    multiple=True,
+    required=True,
+    type=int,
+    help="Durations in seconds ( 31536000 | 94608000 )",
+)
+@click.option(
+    "--purchase_time",
+    required=False,
+    type=str,
+    help="The purchase time of the Savings Plan in UTC format (YYYY-MM-DDTHH:MM:SSZ).",
+)
+@click.option("--client_token", required=False, type=str, help="Client token")
 def savingsplans(
+    purchase_profile,
     region_name,
     commitment,
     durations,
     plan_types,
+    product_type,
     offering_id,
     payment_options,
     client_token,
+    currency,
+    purchase_time,
 ):
-    """ Retrieve SavingsPlans offerings """
-    params = SavingsPlansParams(
-        region_name=region_name,
-        commitment=commitment,
-        offering_id=offering_id,
-        durations=durations,
-        plan_types=plan_types,
-        payment_options=payment_options,
-        client_token=client_token,
-    )
+    """Retrieve SavingsPlans offerings"""
     manager = SavingsPlansManager(region_name=region_name)
-    offerings = manager.get_offering_ids(params)
-    print(json.dumps(offerings, indent=2))
+    """ 1. Get all offerings """
+    params = SavingsPlansParams(
+        offeringIds=offering_id,
+        paymentOptions=payment_options,
+        durations=durations,
+        planTypes=plan_types,
+        productType=product_type,
+        currencies=currency,
+    )
+    offerings = manager.get_offerings(params)
+
+    """ 2. Add Purchase Command and Purchase Offering """
+    purchase_params = SavingsPlansPurchaseParams(
+        purchase_profile=purchase_profile,
+        region_name=region_name,
+        commitment=float(commitment),
+        client_token=client_token,
+        purchase_time=purchase_time,
+    )
+    result = manager.add_keys_to_offerings(offerings, purchase_params)
+
+    print(json.dumps(result, indent=2))
--- src/offering_finder/managers/savingsplans_manager.py
+++ src/offering_finder/managers/savingsplans_manager.py
@@ -3,76 +3,88 @@ import logging
 from typing import Any, Dict, List, Optional
 import boto3
 from offering_finder.clients.AWSClient import AWSClient
-from offering_finder.models.savingsplans_params import SavingsPlansParams
+from offering_finder.models.savingsplans_params import (
+    SavingsPlansParams,
+    SavingsPlansPurchaseParams,
+)
 
 
 class SavingsPlansManager:
-    def __init__(
-        self,
-        region_name: str
-    ) -> None:
+    def __init__(self, region_name: str) -> None:
         self.client = AWSClient("savingsplans", region_name)
 
     def generate_purchase_command(
         self,
-        offering_id: str,
+        purchase_profile: Optional[str],
         region_name: str,
+        offering_id: str,
         commitment: int,
-        client_token: Optional[str] = None
+        purchase_time: Optional[str] = None,
+        client_token: Optional[str] = None,
+        tags: Optional[List[Dict[str, str]]] = None,
     ) -> str:
         """
         Generate the AWS CLI command to purchase a reserved cache node offering.
         """
-        command = (
-            f"aws savingsplans create-savings-plan "
-            f"--region {region_name} "
-            f"--savings-plan-offering-id {offering_id} "
-            f"--commitment {commitment}"
+        command = ""
+        if purchase_profile:
+            command += f"AWS_PROFILE={purchase_profile} "
+        command += (
+            f"aws savingsplans create-savings-plan"
+            f" --region {region_name}"
+            f" --savings-plan-offering-id {offering_id}"
+            f" --commitment {commitment}"
         )
+        if purchase_time:
+            command += f" --purchase-time {purchase_time}"
         if client_token:
             command += f" --client-token {client_token}"
+        if tags:
+            for tag in tags:
+                command += f" --tags Key={tag['Key']},Value={tag['Value']}"
         return command
 
-    def add_keys_to_offering(
-            self,
-            offering: Dict[str, Any],
-            params: SavingsPlansParams
+    def add_keys_to_offerings(
+        self, offerings: Dict[str, Any], params: SavingsPlansPurchaseParams
     ) -> Dict[str, Any]:
-        try:
-            if params.commitment:
+        result = []
+        for offering in offerings:
+            try:
                 offering["OrderCommitment"] = params.commitment
-                offering["OrderEstimatedAmount"] = float(offering["durationSeconds"]) * params.commitment
-            offering["Timestamp"] = datetime.datetime.now(datetime.timezone.utc).isoformat()
-            offering["PurchaseCommand"] = self.generate_purchase_command(
-                offering["offeringId"],
-                params.region_name,
-                params.commitment,
-                params.client_token,
-            )
-            return offering
-        except KeyError as e:
-            logging.error(f"Key error: {e}")
-        except ValueError as e:
-            logging.error(f"Value error: {e}")
-        except Exception as e:
-            logging.error(f"An unexpected error occurred: {e}")
+                offering["OrderEstimatedAmount"] = (
+                    float(offering["durationSeconds"]) * params.commitment
+                )
+                offering["Timestamp"] = datetime.datetime.now(
+                    datetime.timezone.utc
+                ).isoformat()
+                offering["PurchaseCommand"] = self.generate_purchase_command(
+                    offering_id=offering["offeringId"],
+                    purchase_profile=params.purchase_profile,
+                    region_name=params.region_name,
+                    commitment=params.commitment,
+                    client_token=params.client_token,
+                    purchase_time=params.purchase_time,
+                    tags=params.tags,
+                )
+                result.append(offering)
+            except KeyError as e:
+                logging.error(f"Key error: {e}")
+            except ValueError as e:
+                logging.error(f"Value error: {e}")
+            except Exception as e:
+                logging.error(f"An unexpected error occurred: {e}")
         return offering
 
-    def get_offering_ids(
-            self,
-            params: SavingsPlansParams
-    ) -> List[Dict[str, Any]]:
+    def get_offerings(self, params: SavingsPlansParams) -> List[Dict[str, Any]]:
         try:
-            aws_params = params.to_dict()
+            aws_params = params.model_dump(exclude_none=True)
             result = []
             while True:
                 response = self.client.describe_offerings(aws_params)
                 offerings = response.get("searchResults", [])
-                for offering in offerings:
-                    offering = self.add_keys_to_offering(offering, params)
                 result.extend(offerings)
                 if response.get("nextToken"):
-                    """ 次ページが無い場合も 'nextToken': '' という形で返ってくるため、空文字ではない事をチェックする"""
+                    """ 次ページが無い場合も 'nextToken': '' という形で返ってくるため、空文字ではない事をチェック"""
                     aws_params["nextToken"] = response["nextToken"]
                 else:
                     break
